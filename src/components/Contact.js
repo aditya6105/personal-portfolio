846f142169c7dafc13f26fb3087862815292d91b
aditya raj <aditya.rajx77x@gmail.com>
2024-11-22 23:49:00 +0530

    Updated frontend to connect with backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,17 +26,20 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
+    let response = await fetch(
+      "https://personal-portfolio-backend-qya2.onrender.com/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+      }
+    );
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
-    if (result.code == 200) {
+    if (result.code === 200) {
       setStatus({ succes: true, message: "Message sent successfully" });
     } else {
       setStatus({
